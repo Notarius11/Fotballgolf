7b775c6c2b1d7fa91aa9efafcd767e9fe1655218
Notarius11 <reconzinho@gmail.com>
2014-07-23 12:48:43 +0200

    New layout adjustments

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,9 @@ var listOfPlayers,
 	gameStage = 0,
 	typeOfPlay; //1 = Teamplay and 2 equal to individual play
 
+
+
+
 function onLoad() {
 	/* Get the latest news */
 
@@ -15,48 +18,44 @@ function play_click(pTypeOfPlay) {
 	typeOfPlay = pTypeOfPlay;
 	runStep('Next');
 }
+
 function showHideGroup(pControl) {
-	$(pControl).children('.groupContent').show();
+	if ($(pControl).parent().parent().children('.groupContent').css('display') == 'none') {
+		$(pControl).parent().parent().children('.groupContent').show();
+	}else {
+		$(pControl).parent().parent().children('.groupContent').hide();
+	}
 }
 
 /*Tool functions */
-function runStep(pStepDirection) {
-	//First clear of the content
-	if(pStepDirection == 'Next'){
-		$('#gameStage_' + gameStage.toString()).hide();
-		gameStage++;
-		$('#gameStage_' + gameStage.toString()).show();	
-	}
-	else {
-		$('#gameStage_' + gameStage.toString()).hide();
-		gameStage--;
-		$('#gameStage_' + gameStage.toString()).show();	
-	}
+// function runStep(pStepDirection) {
+// 	//First clear of the content
+// 	if(pStepDirection == 'Next'){
+// 		$('#gameStage_' + gameStage.toString()).hide();
+// 		gameStage++;
+// 		$('#gameStage_' + gameStage.toString()).show();	
+// 	}
+// 	else {
+// 		$('#gameStage_' + gameStage.toString()).hide();
+// 		gameStage--;
+// 		$('#gameStage_' + gameStage.toString()).show();	
+// 	}
 
-	if(gameStage == 0){
-		$('#headerLabel').replaceWith('Registration (1 of 3) - Choose type of play');
-		$('#btnPreviousStep').removeClass('btn-enabled');
-		$('#btnPreviousStep').addClass('btn-disabled');
-	} else if (gameStage == 1) {
-		$('#headerLabel').replaceWith('Registration (2 of 3) - Add players');
-		$('#btnPreviousStep').removeClass('btn-disabled');
-		$('#btnPreviousStep').addClass('btn-enabled');
-	}
+// 	if(gameStage == 0){
+// 		$('#headerLabel').replaceWith('Registration (1 of 3) - Choose type of play');
+// 		$('#btnPreviousStep').removeClass('btn-enabled');
+// 		$('#btnPreviousStep').addClass('btn-disabled');
+// 	} else if (gameStage == 1) {
+// 		$('#headerLabel').replaceWith('Registration (2 of 3) - Add players');
+// 		$('#btnPreviousStep').removeClass('btn-disabled');
+// 		$('#btnPreviousStep').addClass('btn-enabled');
+// 	}
+// }
+
+function activateDropdown(pControl) {
+	$(pControl).children('.ui-Dropdown').css('opacity') == 1;
 }
 
-function DropDown(el) {
-    $('#row_1_Team') = el;
-    this.initEvents();
-}
-DropDown.prototype = {
-    initEvents : function() {
-        var obj = this;
- 
-        obj.dd.on('click', function(event){
-            $(this).toggleClass('active');
-            event.stopPropagation();
-        }); 
-    }
-}
+
 
 	
